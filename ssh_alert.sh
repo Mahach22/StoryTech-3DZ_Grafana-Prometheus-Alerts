4a15682fdced7234a9a0e6cc630980122e21f898
mahach <mahach2211@gmail.com>
2025-02-21 17:28:51 +0300

    ssh script update

--- ssh_alert.sh
+++ ssh_alert.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-# Your Email Information: Recipient (To:), Subject and Body
+# Email Information
 RECIPIENT="mahach2211@gmail.com"
-SUBJECT="Email from your Server: SSH Alert"
+SUBJECT="⚠️Обнаружен вход на сервер `uname -n` по ssh⚠"
+FROM="gad000@bk.ru"
 
-BODY="
-Вход по SSH прошел успешно, некоторая информация по безопасности:
-        User:        $PAM_USER
-	User IP Host: $PAM_RHOST
-	Service:     $PAM_SERVICE
-	TTY:         $PAM_TTY
-	Date:        `date`
-	Server:      `uname -a`
-"
+BODY="<html>
+<head>
+    <meta charset='UTF-8'>
+</head>
+<body>
+    <p><b>Информация о входе на сервер:</b></p>
+    <table style='border-collapse: collapse; width: auto;'>
+        <tr><td style='text-align: left; padding-right: 5px;'>Имя сервера:</td> <td><b>`uname -n`</b></td></tr>
+        <tr><td style='text-align: left; padding-right: 5px;'>Использованная учетная запись:</td> <td><b>$PAM_USER</b></td></tr>
+        <tr><td style='text-align: left; padding-right: 5px;'>IP-адрес, с которого подключились:</td> <td><b>$PAM_RHOST</b></td></tr>
+        <tr><td style='text-align: left; padding-right: 5px;'>Дата:</td> <td><b>`date`</b></td></tr>
+    </table>
+</body>
+</html>"
 
-if [ ${PAM_TYPE} = "open_session" ]; then
-	echo "${BODY}" | mail -s "$SUBJECT" -r gad000@bk.ru mahach2211@gmail.com
+
+
+
+
+
+if [ "${PAM_TYPE}" = "open_session" ]; then
+    echo "$BODY" | mailx -a "Content-Type: text/html; charset=UTF-8" -s "$SUBJECT" -r "$FROM" "$RECIPIENT"
 fi
 
 exit 0
-
-
