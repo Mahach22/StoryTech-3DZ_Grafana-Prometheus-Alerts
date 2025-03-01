07815fd852db66d8c4b3ed40dc0f7b1a3ac43d93
Mahach22 <104731301+Mahach22@users.noreply.github.com>
2025-03-01 13:35:20 +0300

    Delete jupyter-volume-size.sh

--- jupyter-volume-size.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-volumes=$(docker volume ls -q | grep jupyter)
-
-for volume in $volumes; do
-  size=$(sudo du -sh $(docker volume inspect --format '{{ .Mountpoint }}' $volume) | awk '{print $1}')
-  echo "Volume: $volume, Size: $size"
-done
